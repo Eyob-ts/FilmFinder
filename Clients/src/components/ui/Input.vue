22b4b456de9fd7bc7f1605fc18a82074488288a3
yob-Branch <yob.teshome27@gmail.com>
2025-05-19 17:48:39 +0300

    feat: integrate authentication with login and registration functionality
    - Added axios instance for API calls with authorization headers.
    - Created auth store using Pinia for managing user authentication state.
    - Implemented login and registration pages with form handling and validation.
    - Added toast notifications for user feedback on authentication actions.
    - Created reusable Button and Input components for UI consistency.
    - Updated main application structure to include router and toastification.
    - Configured CORS settings for API access from the client.
    - Added routes for login and registration in the Vue router.

---
+++ Clients/src/components/ui/Input.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="w-full p-1">
+    <label
+      :for="label"
+      class="block text-sm font-extralight text-gray-500 mb-2"
+    >
+      {{ label }}
+    </label>
+    <input
+      :type="type"
+      :id="label"
+      :placeholder="placeholder"
+      :value="modelValue"
+      @input="$emit('update:modelValue', $event.target.value)"
+      class="
+        w-full
+        px-4
+        py-3
+        font-
+        rounded-xl
+        
+        text-white
+        backdrop-blur-lg
+        
+        placeholder-white/60
+        border
+        focus:outline-none
+        focus:ring-2 
+        transition duration-700
+      "
+    />
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  label: String,
+  type: {
+    type: String,
+    default: 'text'
+  },
+  modelValue: [String, Number],
+  placeholder: {
+    type: String,
+    default: ''
+  }
+})
+
+defineEmits(['update:modelValue'])
+</script>
