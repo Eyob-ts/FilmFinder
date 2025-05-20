b17e2caab014113522d97865f617daa4fd52f4a8
yob-Branch <yob.teshome27@gmail.com>
2025-05-20 14:08:26 +0300

    Your commit message describing the changes

--- Server/config/cors.php
+++ Server/config/cors.php
@@ -1,7 +1,6 @@
 <?php
 
 return [
-<<<<<<< HEAD
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
     'allowed_origins' => ['http://localhost:5173', 'http://localhost:3000'],
@@ -10,36 +9,4 @@ return [
     'exposed_headers' => [],
     'max_age' => 0,
     'supports_credentials' => true,
-=======
-
-    /*
-    |--------------------------------------------------------------------------
-    | Cross-Origin Resource Sharing (CORS) Configuration
-    |--------------------------------------------------------------------------
-    |
-    | Here you may configure your settings for cross-origin resource sharing
-    | or "CORS". This determines what cross-origin operations may execute
-    | in web browsers. You are free to adjust these settings as needed.
-    |
-    | To learn more: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
-    |
-    */
-
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
-
-    'allowed_methods' => ['*'],
-
-    'allowed_origins' => ['*'],
-
-    'allowed_origins_patterns' => [],
-
-    'allowed_headers' => ['*'],
-
-    'exposed_headers' => [],
-
-    'max_age' => 0,
-
-    'supports_credentials' => false,
-
->>>>>>> f2459fa161e10511d226eef39e58ec0ae3c681b0
 ];
