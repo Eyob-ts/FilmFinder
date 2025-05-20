cf7d6ca2a8615acf304495a5baec9a114bc33509
yob-Branch <yob.teshome27@gmail.com>
2025-05-20 15:46:24 +0300

    Resolved routes/api.php conflict

--- Server/routes/api.php
+++ Server/routes/api.php
@@ -21,5 +21,6 @@ Route::post('login', [AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
+    //
     Route::get('/user', [AuthController::class, 'getUser']);
 });
